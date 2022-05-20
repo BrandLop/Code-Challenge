4859c4fff2c44cb63d5debd05a9582d7d07fee7d
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-05-20 17:20:34 -0500

    Adding endpoint that return a visualpartners list

--- test/server.test.js
+++ test/server.test.js
@@ -9,6 +9,22 @@ describe("GET task for VisualPartners API", function() {
         expect(response.body).toMatchObject({message: "VisualPartners API welcome"});
     });
 
+    test("Should return a visualpartners list", async() => {
+        const response = await request(app)
+            .get("/v1/visualpartners");
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.length).toBeGreaterThan(0);
+        expect(response.body[0]).toMatchObject({
+            id: expect.any(String),
+            name: expect.any(String),
+            email: expect.stringMatching(/@/),
+            credits: 508,
+            enrollments: expect.any(Array),
+            previousCourses: expect.any(Number),
+            haveCertification: expect.any(Boolean)
+        });
+    });
+
     afterAll(() => {
         server.close();
     });
